e75aad4d0e701763a32d4414bc3a2ac1c87a5201
SimoneScordato99 <scordato.simone@yahoo.com>
2023-04-10 11:51:22 +0200

    ultimo update

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,21 +19,42 @@ Consigli del giorno:
 
 */
 
-let spazioNumeri = document.querySelector('.container')
+let spazioNumeri = document.getElementById('containerNum')
+let spazioDomanda = document.getElementById('containerAn')
 let scritta = document.querySelector('main')
 let btnGioca = document.getElementById('gioca')
+
+console.log(spazioNumeri)
+console.log(spazioDomanda)
 
 btnGioca.addEventListener('click', function(){
     
     arrayCasuali = []
-    for(let i = 0; i < 5; i++){
+    for(let i = 0; i <5; i++){
         arrayCasuali.push(randomNumber()) 
-        spazioNumeri.innerHTML+=`<div class="box">${arrayCasuali[i]}</div>`
+        spazioNumeri.innerHTML+=`<div class="box">${arrayCasuali[i]}</div>`;
     }
     console.log(arrayCasuali)
     scritta.innerHTML += `<p>ricorda questi 5 numeri</p>`;
-})
 
+    console.log(spazioNumeri.classList)
+        console.log(spazioDomanda.classList)
+
+    setTimeout( function(){
+        console.log('maiale')
+        spazioNumeri.className = 'container none'
+        //spazioNumeri.classList.add('none')
+        //spazioNumeri.classList.remove('active')
+    
+        spazioDomanda.className = 'container active'
+        //spazioDomanda.classList.add('active')
+        //spazioDomanda.classList.remove('none')
+    
+        console.log(spazioNumeri.classList)
+        console.log(spazioDomanda.classList)
+    
+    }, 2000)
+})
 
 
 
